cffee234a3dd21a76cf57f415aa0d811029bd21d
Brahim <cb_bousssadjra@esi.dz>
2020-12-19 22:35:20 +0100

    fix check mark position and the sidebar height

--- src/views/guide/Index.vue
+++ src/views/guide/Index.vue
@@ -1,25 +1,23 @@
 <template>
 <div class="flex flex-row ">
 
-    <transition name="slide-fade">
+    <aside class=" md:flex flex-col bg-green-50 py-6 w-64 sticky min-h-screen  shadow-lg">
+        <div class="text-gray-600 text-xl font-bold px-4 py-2">
+            Guide
+        </div>
+        <div class="flex w-full py-4 ">
 
-        <aside class=" md:flex flex-col bg-green-50 py-6 w-64 sticky min-h-screen  shadow-lg">
-            <div class="text-gray-600 text-xl font-bold px-4 py-2">
-                Guide
-            </div>
-            <div class="flex w-full py-4 ">
+            <ul class="flex flex-col w-full  overflow-auto">
+                <li class="relative w-full " v-for="(component, index) in components" :key="index">
+                    <router-link active-class="text-green-700 bg-green-200" class="relative flex  text-gray-600 w-full pl-4 p-2 items-center" :to="'/guide/'+component.path">
 
-                <ul class="flex flex-col w-full max-h-96 overflow-auto">
-                    <li class="relative w-full " v-for="(component, index) in components" :key="index">
-                        <router-link active-class="text-green-700 bg-green-200" class="relative flex  text-gray-600 w-full pl-4 p-2 items-center" :to="'/guide/'+component.path">
+                        <span class="text-sm py-1 whitespace-nowrap">{{ component.name }}</span>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </aside>
 
-                            <span class="text-sm py-1 whitespace-nowrap">{{ component.name }}</span>
-                        </router-link>
-                    </li>
-                </ul>
-            </div>
-        </aside>
-    </transition>
     <section class="w-full transition-all overflow-x-hidden" style="overflow-y: auto; max-height: calc(100vh - 64px)">
         <div class="w-full items-center sticky top-0 z-20 flex bg-gray-100">
 
